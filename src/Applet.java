8c22cd0879518c31e9c4b67dc87217346b0aff21
Chris Bao <chrisbaome@gmail.com>
2020-03-29 13:56:32 -0400

    Made the Applet and Board classes import javax.swing instead of using the full names

--- src/Applet.java
+++ src/Applet.java
@@ -1,10 +1,12 @@
+import javax.swing.*;
+
 /**
  * Applet class. This represents the game.
  * @author Chris W. Bao, Ben Megan
  * @since 28 MAR 2020
  * @version 0.1.1
  */
-class Applet extends javax.swing.JApplet {
+class Applet extends JApplet {
     // FIELDS
     Board board;
     
--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,12 @@
+import javax.swing.*;
+
 /**
  * Board class. The chessboard is stored and displayed inside this.
  * @author Chris W. Bao, Ben Megan
  * @since 28 MAR 2020
  * @version 0.1.1
  */
-class Board extends javax.swing.JPanel {
+class Board extends JPanel {
 	// FIELDS
 	
 	// CONSTRUCTORS
