9f09c05a319f2896c43132502fab6a8698f736f6
Chris Bao <chrisbaome@gmail.com>
2020-03-29 12:58:02 -0400

    Added some comments and Javadoc comments, no substantive changes

--- src/Applet.java
+++ src/Applet.java
@@ -1,4 +1,14 @@
+/**
+ * Applet class. This represents the game.
+ * @author Chris W. Bao, Ben Megan
+ * @since 28 MAR 2020
+ * @version 0.1.1
+ */
 class Applet extends javax.swing.JApplet {
     // FIELDS
     Board board;
+    
+    // CONSTRUCTORS
+    
+    // METHODS
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,16 @@
+/**
+ * Driver class. The game is run through here.
+ * @author Chris W. Bao, Ben Megan
+ * @since 28 MAR 2020
+ * @version 0.1.1
+ */
 public class Driver {
-    // MAIN()
-    public static void main(String[] args) {
-        Applet applet = new Applet();
-    }
+	// MAIN()
+	/**
+	 * Main() definition. Game launches through this.
+	 * @param args arguments passed through command line
+	 */
+	public static void main(String[] args) {
+		Applet applet = new Applet();
+	}
 }
