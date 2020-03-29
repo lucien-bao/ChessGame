6df07293fe1e67bceb32785e8b3e0bd14a7855cd
Chris Bao <chrisbaome@gmail.com>
2020-03-29 14:02:07 -0400

    Some cleanup

--- src/Board.java
+++ src/Board.java
@@ -4,7 +4,7 @@ import javax.swing.*;
  * Board class. The chessboard is stored and displayed inside this.
  * @author Chris W. Bao, Ben Megan
  * @since 28 MAR 2020
- * @version 0.1.1
+ * @version 0.1.2
  */
 class Board extends JPanel {
 	// FIELDS
--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,14 @@
 /**
- * Piece class. Types of pieces and their possible moves
+ * Piece class. Types of pieces and their possible moves.
  * @author Chris W. Bao, Ben Megan
- * @since 28 MAR 2020
+ * @since 29 MAR 2020
  * @version 0.1.1
  */
-public class Piece {
+class Piece {
     // FIELDS
     boolean isWhite;
     int type;
+    
     // CONSTRUCTORS
 
     // METHODS
