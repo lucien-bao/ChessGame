06c1b8184dbb5a3f6c5907f0b6ad11d1f0e941cf
Chris Bao <chrisbaome@gmail.com>
2020-04-03 14:17:32 -0400

    Added ComponentListener to detect JFrame resize

--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Frame class. This represents the game.
@@ -7,22 +8,27 @@ import java.awt.*;
  * @since 28 MAR 2020
  * @version 0.1.2
  */
-class Frame extends JFrame {
+class Frame extends JFrame implements ComponentListener {
     // CONSTANTS
-    final int WIDTH  = 800;
-    final int HEIGHT = 800;
+    final int DEFAULT_WIDTH = 800;
+    final int DEFAULT_HEIGHT = 800;
+    Dimension size = new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
     // FIELDS
-    BoardPanel boardPanel;
-    InfoPanel infoPanel;
-    PlayerPanel blackPanel;
-    PlayerPanel whitePanel;
+    BoardPanel boardPanel;  // Chessboard panel
+    InfoPanel infoPanel;    // General info panel
+    PlayerPanel blackPanel; // Black player info panel
+    PlayerPanel whitePanel; // White player info panel
 
     // CONSTRUCTOR
     /**
      * Constructor. Initiates all the JPanels.
      */
     Frame() {
+        // Resizing stuff
+        addComponentListener(this);
+        
+        // Panel stuff
         boardPanel = new BoardPanel();
         boardPanel.setBackground(Color.PINK);
         infoPanel  = new InfoPanel();
@@ -35,11 +41,37 @@ class Frame extends JFrame {
         this.add(infoPanel, BorderLayout.NORTH);
         this.add(blackPanel, BorderLayout.WEST);
         this.add(whitePanel, BorderLayout.EAST);
-        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        
+        // Misc. setup
+        this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
     }
 
     // METHODS
+    /**
+     * Resizes the panels after the JFrame instance gets resized.
+     */
+    void resizePanels() {
+        //TODO: implement
+    }
+    
+    // COMPONENTLISTER METHODS
+    /**
+     * Changes the [size] field when the window gets resized
+     * @param e the event passed to this methods on resizing
+     */
+    @Override
+    public void componentResized(ComponentEvent e) {
+        size = e.getComponent().getSize();
+        resizePanels();
+    }
+    
+    @Override
+    public void componentMoved(ComponentEvent e) {}
+    @Override
+    public void componentShown(ComponentEvent e) {}
+    @Override
+    public void componentHidden(ComponentEvent e) {}
 }
